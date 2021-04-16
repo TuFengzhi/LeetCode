c793c52d7a0f48c65c74fe0135ab5eae1ae7d449
Tu Fengzhi <tfz@hkincus.com>
2021-04-16 15:22:31 +0800

    remove file

--- C/q0665_checkPossibility.c
+++ C/q0665_checkPossibility.c
@@ -1,3 +1,10 @@
+// 665. Non-decreasing Array
+// Given an array nums with n integers, your task is to check if it could become
+// non-decreasing by modifying at most one element.
+//
+// We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for
+// every i (0-based) such that (0 <= i <= n - 2).
+
 #include "ctest.h"
 #include "leetcode.h"
 
