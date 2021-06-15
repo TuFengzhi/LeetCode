2a70c6ecca93b3685bb6d0f75390c0522269d325
Tu Fengzhi <tfz@hkincus.com>
2021-06-15 17:52:03 +0800

    Add #404 Code

---
+++ C++/q0404_sumOfLeftLeaves.cpp
@@ -0,0 +1,46 @@
+// 404. Sum of Left Leaves
+// Given the root of a binary tree, return the sum of all left leaves.
+
+#include "leetcode.h"
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int sumOfLeftLeaves(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int sum = 0;
+
+        if (root->left)
+            sum += helper(root->left, root);
+        if (root->right)
+            sum += helper(root->right, root);
+        return sum;
+    }
+
+  private:
+    int helper(TreeNode *root, TreeNode *prev)
+    {
+        int sum = 0;
+        if (!root->left && !root->right)
+        {
+            if (prev->left == root)
+                return root->val;
+            else
+                return 0;
+        }
+        if (root->left)
+        {
+            sum += helper(root->left, root);
+        }
+        if (root->right)
+        {
+            sum += helper(root->right, root);
+        }
+        return sum;
+    }
+};
