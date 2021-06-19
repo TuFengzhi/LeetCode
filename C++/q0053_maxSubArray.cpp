326797beef3d5e3b6e9c586b23831e54671c7bf7
Tu Fengzhi <tufengzhi@gmail.com>
2021-06-19 18:16:24 +0800

    Add #53 C++ Solution

---
+++ C++/q0053_maxSubArray.cpp
@@ -0,0 +1,26 @@
+// 53. Maximum Subarray
+// Given an integer array nums, find the contiguous subarray (containing at
+// least one number) which has the largest sum and return its sum.
+
+#include "leetcode.h"
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int max_sum = nums[0];
+        int current_sum = 0;
+        for (auto &e : nums)
+        {
+            current_sum = max(current_sum + e, e);
+            max_sum = max(max_sum, current_sum);
+        }
+
+        return max_sum;
+    }
+
+    // TODO: Add Divide and Conquer Solution
+};
