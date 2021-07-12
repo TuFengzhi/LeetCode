d7a7ed5e9531e86c947e608558b1183be735e848
Tu Fengzhi <tfz@hkincus.com>
2021-07-12 17:51:08 +0800

    Add #309 C++ Solution

--- C++/q0309_maxProfit.cpp
+++ C++/q0309_maxProfit.cpp
@@ -20,6 +20,19 @@ class Solution
   public:
     int maxProfit(vector<int> &prices)
     {
-        // TODO:
+        int size = prices.size();
+        int dp[size][3]; // 0: Own Stock, 1: Cool Down, 2: Not Own Stock && Not
+                         // Cool Down
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        for (int i = 1; i < size; i++)
+        {
+            dp[i][0] = max(dp[i - 1][2] - prices[i], dp[i - 1][0]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2]);
+        }
+
+        return max(dp[size - 1][1], dp[size - 1][2]);
     }
 };
