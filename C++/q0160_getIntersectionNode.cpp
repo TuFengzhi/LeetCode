33c4786705bc072287d4ed75f05fc84043666f10
zubeneschamali <tufengzhi@gmail.com>
2021-07-14 18:27:23 +0800

    Add New Solutiom (#2)
    * Add TODO
    
    * Add #279 C++ Solution
    
    * Add #300 C++ Solution
    
    * Add #309 C++ Solution
    
    * Add #14 C++ Solution
    
    * Add #26 C++ Solution
    
    * Add #88 C++ Solution
    
    * Add #122 C++ Solution
    
    * Add #155 C++ Solution
    
    * Add #160 C++ Solution
    
    * ADD #169 C++ Solution
    
    * Add #206 C++ Solution
    
    * Add #557 C++ Solution
    
    * Update #557 C++ Solution
    
    Co-authored-by: Tu Fengzhi <tfz@hkincus.com>

---
+++ C++/q0160_getIntersectionNode.cpp
@@ -0,0 +1,66 @@
+// 160. Intersection of Two Linked Lists
+//
+// Given the heads of two singly linked-lists headA and headB, return the node
+// at which the two lists intersect. If the two linked lists have no
+// intersection at all, return null.
+
+#include "leetcode.h"
+
+using namespace std;
+
+class Solution
+{
+  public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        int len_a = 0, len_b = 0;
+        ListNode *itr_a = headA, *itr_b = headB;
+
+        while (itr_a)
+        {
+            len_a++;
+            itr_a = itr_a->next;
+        }
+        while (itr_b)
+        {
+            len_b++;
+            itr_b = itr_b->next;
+        }
+
+        itr_a = headA;
+        itr_b = headB;
+
+        if (len_a > len_b)
+        {
+            for (int i = 0; i < len_a - len_b; i++)
+            {
+                itr_a = itr_a->next;
+            }
+        }
+        else if (len_a < len_b)
+        {
+            for (int i = 0; i < len_b - len_a; i++)
+            {
+                itr_b = itr_b->next;
+            }
+        }
+
+        int len = min(len_a, len_b);
+        ListNode *ret = NULL;
+        for (int i = 0; i < len; i++)
+        {
+            if (itr_a == itr_b)
+            {
+                ret = itr_a;
+                break;
+            }
+            else
+            {
+                itr_a = itr_a->next;
+                itr_b = itr_b->next;
+            }
+        }
+
+        return ret;
+    }
+};
---
+++ C++/q0557_reverseWords.cpp
@@ -0,0 +1,52 @@
+// 557. Reverse Words in a String III
+// Given a string s, reverse the order of characters in each word within a
+// sentence while still preserving whitespace and initial word order.
+
+#include "leetcode.h"
+
+using namespace std;
+
+class Solution
+{
+  public:
+    string reverseWords(string s)
+    {
+        int len = s.size();
+        int start = -1, end = -1, finish = 1;
+        for (int i = 0; i < len; i++)
+        {
+            if (s[i] == ' ')
+            {
+                while (start < end)
+                {
+                    swap(s[start], s[end]);
+
+                    start++;
+                    end--;
+                }
+                end = start;
+                finish = 1;
+            }
+            else if (finish)
+            {
+                start = i;
+                end = i;
+                finish = 0;
+            }
+            else
+            {
+                end = i;
+            }
+        }
+
+        while (start < end)
+        {
+            swap(s[start], s[end]);
+
+            start++;
+            end--;
+        }
+
+        return s;
+    }
+};
